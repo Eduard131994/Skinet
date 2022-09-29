3f39ca2d571a0b97a5acaad53660b3d5bba93202
GhEduard <eduard.ghiurca@de.rhenus.com>
2022-09-29 08:02:53 +0200

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,13 +1,24 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<StoreContext>(x => x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dataContext = scope.ServiceProvider.GetRequiredService<StoreContext>();
+    await dataContext.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(dataContext);
+}
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
